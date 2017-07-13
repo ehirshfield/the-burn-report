1d02d3a862304ec15dedd785f6a4817782d2fbce
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2017-07-12 23:20:01 -0700

    Set up first form component

---
+++ src/Parser.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Parser extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      rawData: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render(){
+    return(
+      <form>
+        <textarea type="text" value={this.state.rawData} name="rawData" placeholder="Put raw text here to parse" onChange={this.handleInputChange} />
+        <button id="parseButton" type="submit" onClick={this.handleSubmit}>PARSE IT GOOD</button>
+      </form>
+    )
+  }
+}
+
+export default Parser;
